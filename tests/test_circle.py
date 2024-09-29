4769e358c74695db7217e2cf73b9f5aa60a3d142
Natalia Shekhman <natalya.shekhman@vitech.team>
2024-09-29 20:10:40 +0300

    pytest_lesson/check ruff format

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,56 +1,46 @@
 import pytest
 from circle import Circle
-@pytest.mark.parametrize("radius,area",
-    [
-        (3, 28.274333882308138),
-        (3.5, 38.48451000647496)
-    ],
-    ids=['integer', 'float']
-)
 
+
+@pytest.mark.parametrize(
+    "radius,area",
+    [(3, 28.274333882308138), (3.5, 38.48451000647496)],
+    ids=["integer", "float"],
+)
 @pytest.mark.smoke
 @pytest.mark.circle
 def test_circle_area_positive(radius, area):
     r = Circle(radius)
-    assert r.area == area, f'Площадь должна быть {area}'
+    assert r.area == area, f"Площадь должна быть {area}"
 
-@pytest.mark.parametrize("radius,area",
-    [
-        (3, 28.27),
-        (3.5, 39.48451000647496)
-    ],
-    ids=['integer', 'float']
+
+@pytest.mark.parametrize(
+    "radius,area", [(3, 28.27), (3.5, 39.48451000647496)], ids=["integer", "float"]
 )
-
 @pytest.mark.negative
 @pytest.mark.circle
 def test_circle_area_negative(radius, area):
     r = Circle(radius)
-    assert r.area != area, f'Площадь рассчитана некорректно'
+    assert r.area != area, f"Площадь рассчитана некорректно"
 
 
-@pytest.mark.parametrize("radius,perimeter",
-    [
-        (3, 18.84955592153876),
-        (3.5, 21.991148575128552)
-    ],
-    ids=['integer', 'float']
+@pytest.mark.parametrize(
+    "radius,perimeter",
+    [(3, 18.84955592153876), (3.5, 21.991148575128552)],
+    ids=["integer", "float"],
 )
 @pytest.mark.smoke
 @pytest.mark.circle
 def test_circle_perimeter_positive(radius, perimeter):
     r = Circle(radius)
-    assert r.perimeter == perimeter, f'Периметр должен быть равен {perimeter}'
+    assert r.perimeter == perimeter, f"Периметр должен быть равен {perimeter}"
 
-@pytest.mark.parametrize("radius,perimeter",
-    [
-        (3, 19.8495),
-        (3.5, 23.9911485)
-    ],
-    ids=['integer', 'float']
+
+@pytest.mark.parametrize(
+    "radius,perimeter", [(3, 19.8495), (3.5, 23.9911485)], ids=["integer", "float"]
 )
 @pytest.mark.negative
 @pytest.mark.circle
 def test_circle_perimeter_negative(radius, perimeter):
     r = Circle(radius)
-    assert r.perimeter != perimeter, f'Периметр рассчитан некорректно'
+    assert r.perimeter != perimeter, f"Периметр рассчитан некорректно"
--- tests/test_square.py
+++ tests/test_square.py
@@ -1,62 +1,45 @@
 import pytest
 from square import Square
-@pytest.mark.parametrize("side_a, area",
-    [
-        (4, 16),
-        (3.5, 12.25)
-    ],
-    ids=['integer', 'float']
-)
 
+
+@pytest.mark.parametrize(
+    "side_a, area", [(4, 16), (3.5, 12.25)], ids=["integer", "float"]
+)
 @pytest.mark.smoke
 @pytest.mark.square
 @pytest.mark.positive
 def test_square_area_positive(side_a, area):
     s = Square(side_a)
-    assert s.area == area, f'Площадь должна быть {area}'
+    assert s.area == area, f"Площадь должна быть {area}"
 
-@pytest.mark.parametrize("side_a, area",
-    [
-        (4, 15),
-        (3.5, 15.25)
-    ],
-    ids=['integer', 'float']
+
+@pytest.mark.parametrize(
+    "side_a, area", [(4, 15), (3.5, 15.25)], ids=["integer", "float"]
 )
-
 @pytest.mark.smoke
 @pytest.mark.square
 @pytest.mark.negative
 def test_square_area_negative(side_a, area):
     s = Square(side_a)
-    assert s.area != area, f'Площадь рассчитан неверно'
+    assert s.area != area, f"Площадь рассчитан неверно"
 
-@pytest.mark.parametrize("side_a, perimeter",
-    [
-        (4, 16),
-        (3.5, 14)
-    ],
-    ids=['integer', 'float']
+
+@pytest.mark.parametrize(
+    "side_a, perimeter", [(4, 16), (3.5, 14)], ids=["integer", "float"]
 )
-
-
 @pytest.mark.smoke
 @pytest.mark.square
 @pytest.mark.positive
 def test_square_perimeter_positive(side_a, perimeter):
-   s = Square(side_a)
-   assert s.perimeter == perimeter, f'Периметр должен быть равен {perimeter}'
+    s = Square(side_a)
+    assert s.perimeter == perimeter, f"Периметр должен быть равен {perimeter}"
 
-@pytest.mark.parametrize("side_a, perimeter",
-    [
-        (4, 14),
-        (3.5, 12)
-    ],
-    ids=['integer', 'float']
+
+@pytest.mark.parametrize(
+    "side_a, perimeter", [(4, 14), (3.5, 12)], ids=["integer", "float"]
 )
-
-
 @pytest.mark.square
 @pytest.mark.negative
 def test_square_perimeter_negative(side_a, perimeter):
     s = Square(side_a)
-    assert s.perimeter != perimeter, f'Периметр рассчитан неверно'
+    assert s.perimeter != perimeter, f"Периметр рассчитан неверно"
